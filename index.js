55156aac175e9d29fc24f5732d178871f8b923b9
lixiaochun <490398311@qq.com>
2018-03-02 20:29:12 +0800

    update 采用promise写法的简单爬虫

--- index.js
+++ index.js
@@ -2,73 +2,48 @@ const request = require('superagent')
 const cheerio = require('cheerio')
 const h2m = require('h2m')
 const fs = require('fs')
+const Q = require('q')
 
 // 定义请求url的基本样式
-const url = 'http://ife.baidu.com/course/detail/id/'
-const dist = './md/'
+const url = 'http://ife.baidu.com'
+const root = './dist/'
 
-let flag = false
-let college = []
-let Count = 1
+let mkdir = Q.denodeify(fs.mkdir)
 
-// md对象的构造函数
-function MdObject(path, title, order, content ) {
-  this.path = path
-  this.title = title
-  this.order = order
-  this.content = content
-}
-
-for (let i=1; i<200; i++) {
-  let order = 1
-  request
-  .get(url + i)
-  .end((err, res) => {
-    // 错误处理
-    if(err) {
-      console.log('请求太快了...')
-      i--
-    }
-      // 请求内容源
-    let $ = cheerio.load(res.text, {decodeEntities: false}) // 加参数解决中文乱码问题
-    let main = $('main')
-    let obj = new MdObject()
-    
-
-    // 跳出处理
-    if(main.hasClass('not-found-wrap')) {
-      return false
-    }
-    // 处理请求到的内容
-    $('.deadline-tip').remove() // 删掉截止日期
-
-    let path = main.find('div .course-from a').eq(1).text()
-    // college.push(path)
-    if(college.indexOf(path) === -1) { // 当前遍历是新的文件夹
-      college.push(path)
-      obj.path = path
-      order = 1
-      fs.mkdir(dist + path, (err) => {
-        if(err){
-          console.log('文件夹建立错误...')
-         }else{
-          console.log("建立" + path + '文件夹')
-         }
-      })
-      
-    } else {
-      obj.path = path
-      order++
-    }
-
-    // 对标题格式进行检查
-    let title = main.find('.course-title').text().trim().replace('\/', ' or ')
-    obj.title = title + '.md'
-    let html = main.find('.md-content-wrap').html()
-    let md = h2m(html)
-
-    fs.writeFile(dist + obj.path + '/' + obj.title, md, err => {
-      if(err) throw err      
+mkdir(root)
+.then(() => {
+  return request(url)
+})
+.then(res => {
+  let colleges = []
+  let $ = cheerio.load(res.text, {decodeEntities: false}) // 加参数解决中文乱码问题
+  $('.home-college-item').each(function(index, element) {
+    let $element = $(element)
+    colleges.push({
+      title: $element.find('.college-content-title').text(),
+      href: $element.find('a').attr('href')
     })
   })
-}
+  return colleges
+})
+.then(colleges => {
+  let promise = Promise.resolve()
+  colleges.forEach(college => {
+    promise = promise.then(() => {
+      return mkdir('./dist/'+ college.title)
+    })
+  })
+  return colleges
+})
+.then(colleges => {
+  let promise = Promise.resolve()
+  colleges.map(college => {
+    return request(url + college.href)
+  })
+})
+.then(res => {
+  //
+})
+.catch(err => {
+  console.log(err.stack)
+})
